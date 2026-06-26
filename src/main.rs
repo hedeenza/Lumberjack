92fc70bc2a1fb4cf593ab4fbb6bd749be9b897f5
Zach Hedeen <hedeenza@gmail.com>
2026-06-26 09:21:53 -0400

    extracted additional functions

--- src/main.rs
+++ src/main.rs
@@ -28,57 +28,10 @@ fn main() -> ExitCode {
     // Chunk vector into desired size
     let timber = size_forest(&args.lines, &forest);
 
-    // Create a directory to hold the output
-    // Find the position of the period in the input file name, if there is one
-    let period_index = match args.input.find(".") {
-        Some(index) => index,
-        None => args.input.len(),
-    };
-    // The output name is everything up to the period index
-    let input_name = &args.input[..period_index];
+    // Create output directory if it does not exist
+    let mill = manage_destination(&args.input);
 
-    // Destination directory includes the input file name for ease of location
-    let mill = String::from("./") + input_name + &String::from("_divided/");
-    
-    // If the directory already exists, do nothing
-    if Path::new(&mill).exists() {
-        ()
-    // If the directory does not exist, create it and 
-    // wait for creation to finish before moving on
-    } else {
-        let mut create_directory = Command::new("mkdir")
-            .arg(&mill)
-            .spawn().
-            expect("Could not create output directory");
-        let _child = create_directory
-            .wait()
-            .expect("Could not wait for directory creation");
-    }
-
-    // Initialize line counter
-    let mut i = 0;
-
-    for log in timber {
-        // Shift 0-index to a more human-sensible index
-        i += 1;
-        // Create a second value, j, equal to i
-        let mut j = i;
-        // Add the lengh of the log to j, subtract 1 so it equals the line value
-        j += log.len() - 1;
-        // The output extension is everything after and including the period index
-        let input_extension = &args.input[period_index..];
-        // Format the output name to include the original file name, the included line range, and
-        // original extension if there was one
-        let output_file = format!("{}_{}-{}{}", input_name, i, j, input_extension);
-        // Create the output file
-        let mut final_output = File::create(mill.clone() + &output_file).expect("Failed to Create Output File");
-        // Write each line in the chunk to the proper output file
-        for ring in log {
-            let _ = writeln!(final_output, "{}", ring);
-        }
-        // Set i to j so the next loop starts where this one ended
-        i = j
-    }
+    chop_lumber(&args.input, timber, mill);
 
     ExitCode::from(0)
 }
@@ -121,4 +74,68 @@ fn size_forest<'a>(input_size: &'a usize, forest: &'a Vec<String>) -> Vec<&'a [S
         .collect();
 
     timber
+}
+
+fn manage_destination(input_file: &str) -> String {
+    // Create a directory to hold the output
+    // Find the position of the period in the input file name, if there is one
+    let period_index = match input_file.find(".") {
+        Some(index) => index,
+        None => input_file.len(),
+    };
+    // The output name is everything up to the period index
+    let input_name = &input_file[..period_index];
+
+    // Destination directory includes the input file name for ease of location
+    let mill = String::from("./") + input_name + &String::from("_divided/");
+    
+    // If the directory already exists, do nothing
+    if Path::new(&mill).exists() {
+        ()
+    // If the directory does not exist, create it and 
+    // wait for creation to finish before moving on
+    } else {
+        let mut create_directory = Command::new("mkdir")
+            .arg(&mill)
+            .spawn().
+            expect("Could not create output directory");
+        let _child = create_directory
+            .wait()
+            .expect("Could not wait for directory creation");
+    }
+    mill
+}
+
+fn chop_lumber(input_file: &str, timber: Vec<&[String]>, mill: String) {
+    // Initialize line counter
+    let mut i = 0;
+
+    for log in timber {
+        // Shift 0-index to a more human-sensible index
+        i += 1;
+        // Create a second value, j, equal to i
+        let mut j = i;
+        // Add the lengh of the log to j, subtract 1 so it equals the line value
+        j += log.len() - 1;
+        // Find the position of the period in the input file name, if there is one
+        let period_index = match input_file.find(".") {
+            Some(index) => index,
+            None => input_file.len(),
+        };
+        // The output name is everything up to the period index
+        let input_name = &input_file[..period_index];
+        // The output extension is everything after and including the period index
+        let input_extension = &input_file[period_index..];
+        // Format the output name to include the original file name, the included line range, and
+        // original extension if there was one
+        let output_file = format!("{}_{}-{}{}", input_name, i, j, input_extension);
+        // Create the output file
+        let mut final_output = File::create(mill.clone() + &output_file).expect("Failed to Create Output File");
+        // Write each line in the chunk to the proper output file
+        for ring in log {
+            let _ = writeln!(final_output, "{}", ring);
+        }
+        // Set i to j so the next loop starts where this one ended
+        i = j
+    }
 }

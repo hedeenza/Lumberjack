a25649b1b0a9bf853ba194a2ecd5ac00c796119f
Zach Hedeen <hedeenza@gmail.com>
2026-06-26 08:14:13 -0400

    Added for loop to handle included line number tracking, creation of output files with notation for included line number range, and writing to output files. Reformatted lines

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 
 fn main() {
 
-    let file = File::open("test.txt");
+    let input = "second_test";
+
+    let file = File::open(input);
 
     let file_reader = match file {
         Ok(file) => BufReader::new(file),
@@ -14,12 +16,8 @@ fn main() {
 
     for line in file_reader.lines() {
         match line {
-            Ok(line) => {
-                forest.push(line);
-            }
-            Err(err) => {
-                println!("[ ERROR ]: {}", err);
-            }
+            Ok(line) => forest.push(line),
+            Err(err) => println!("[ ERROR ]: {}", err),
         }
     }
 
@@ -32,4 +30,36 @@ fn main() {
     let timber: Vec<&[String]> = forest
         .chunks(log_size)
         .collect();
+
+    // Initialize line counter
+    let mut i = 0;
+
+    for log in timber {
+        // Shift 0-index to a more human-sensible index
+        i += 1;
+        // Create a second value, j, equal to i
+        let mut j = i;
+        // Add the lengh of the log to j, subtract 1 so it equals the line value
+        j += log.len() - 1;
+        // Find the position of the period in the input file name, if there is one
+        let period_index = match input.find(".") {
+            Some(index) => index,
+            None => input.len(),
+        };
+        // The output name is everything up to the period index
+        let output_name = &input[..period_index];
+        // The output extension is everything after and including the period index
+        let output_extension = &input[period_index..];
+        // Format the output name to include the original file name, the included line range, and
+        // original extension if there was one
+        let output_file = format!("{}_{}-{}{}", output_name, i, j, output_extension);
+        // Create the output file
+        let mut final_output = File::create(&output_file).expect("Failed to Create Output File");
+        // Write each line in the chunk to the proper output file
+        for ring in log {
+            let _ = writeln!(final_output, "{}", ring);
+        }
+        // Set i to j so the next loop starts where this one ended
+        i = j
+    }
 }
